a9034ff48017fa4df93af3908741dbac6c26ea14
nicolahcm <nicolahu1997@gmail.com>
2021-01-30 15:55:18 +0100

    new

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-require('dotenv').config()
 
 
 const Person = require('./models/person')
